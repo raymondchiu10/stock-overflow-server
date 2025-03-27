a7cebeb4dd9b356c45c8f2c8d1a3df5d2ee6c636
Raymond Chiu <raymondchiu10@gmail.com>
2025-03-27 09:42:29 -0700

    remove console.log();

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,6 @@
 import pg from "pg";
 import "dotenv/config";
 
-console.log("process.env.DB_CONNECTION_STRING", process.env.DB_CONNECTION_STRING);
-
 const pool = new pg.Pool({
 	connectionString: process.env.DB_CONNECTION_STRING,
 	ssl: false,
