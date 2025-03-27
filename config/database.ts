722cdab1b52da79dc491ba5240dccfcc68a98b74
Raymond Chiu <raymondchiu10@gmail.com>
2025-03-27 09:42:00 -0700

    update auth

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,11 @@
 import pg from "pg";
 import "dotenv/config";
 
+console.log("process.env.DB_CONNECTION_STRING", process.env.DB_CONNECTION_STRING);
+
 const pool = new pg.Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-  ssl: false,
+	connectionString: process.env.DB_CONNECTION_STRING,
+	ssl: false,
 });
 
 export default pool;
