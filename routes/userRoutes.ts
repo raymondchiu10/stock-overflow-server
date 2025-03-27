74080fc0f4c59db6a75cbd3b76cbeca3837edecb
Raymond Chiu <raymondchiu10@gmail.com>
2025-03-26 17:35:32 -0700

    implement jwt

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,7 +1,15 @@
+import bcrypt from "bcryptjs";
 import { Router, type Request, type Response } from "express";
 
 const userRoutes = Router();
 
-userRoutes.route("/").post().get();
+const users: { username: string; password: string }[] = [];
+
+userRoutes.route("/register").post(async (req: Request, res: Response) => {
+	const { username, password } = req.body;
+	const hashedPassword = await bcrypt.hash(password, 10);
+	users.push({ username, password: hashedPassword });
+	res.json({ message: "User registered successfully!" });
+});
 
 export default userRoutes;
